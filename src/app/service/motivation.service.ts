eee726de9ed66552e961eec7af04a217c669f4f8
nokizemaxime <93329117+nokizemaxime@users.noreply.github.com>
2023-05-17 09:46:54 +0100

    add service motivation

--- src/app/service/motivation.service.ts
+++ src/app/service/motivation.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MotivationService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+   /**
+ * Motivation
+ * API
+ * Services 
+ * Starts
+ */
+    postMotivation(data: any) {
+      return this.http.post<any>('http://localhost:8000/api/Motivation', data).pipe(
+        map((res: any) => {
+          return res;
+        })
+      );
+    }
+  
+    getMotivation() {
+      return this.http.get<any>('http://localhost:8000/api/Motivation').pipe(
+        map((res: any) => {
+          return res;
+        })
+      );
+    }
+  
+    updateMotivation(data: any,id:number){
+      return this.http.put<any>('http://localhost:8000/api/Motivation/'+id,data).pipe(map((res:any)=>{
+        return res;
+      }))
+    }
+  
+    deleteMotivation(id:number){
+      return this.http.delete<any>("http://localhost:8000/api/Motivation/"+id).pipe(map((res:any)=>{
+        return res;
+      }))
+    }
+  }
 }
